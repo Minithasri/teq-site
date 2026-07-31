dd46302b1b72e53f58a0ffcb9a8680325c5d4754
kaviyapriyap <kaviya.priya@gwcdata.ai>
2026-07-31 15:59:12 +0530

    updated code

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,7 +7,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <head>
-        <link rel='icon' href='/favicon.ico' sizes='any' />
+        <link rel='icon' href='/images/landing_logo.png' type='image/png' />
       </head>
       <body className='flex flex-col min-h-screen'>
         <Providers>
